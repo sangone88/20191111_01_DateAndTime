fa7ea4f903e1413abb6f03af3bff4736e8e5b882
sangone88 <llil1@naver.com>
2019-11-11 20:39:38 +0900

    얼럿다이얼로그 연습

--- app/src/main/java/com/tjoeun/a20191111_01_dateandtime/EditScheduleActivity.kt
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/EditScheduleActivity.kt
@@ -1,8 +1,11 @@
 package com.tjoeun.a20191111_01_dateandtime
 //EditScheduleActivity
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit_schedule.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -22,6 +25,20 @@ class EditScheduleActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        saveBtn.setOnClickListener {
+            var alert = AlertDialog.Builder(this)
+
+            alert.setTitle("저장확인")
+            alert.setMessage("정말 일정을 저장하겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                Toast.makeText(this, "일정을 저장했습니다.", Toast.LENGTH_SHORT).show()
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+        }
+
         startDateBtn.setOnClickListener { 
             var datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
 
