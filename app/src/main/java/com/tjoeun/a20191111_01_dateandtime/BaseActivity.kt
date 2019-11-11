45fcc55385125c35afe7a8523be89d52d3f00799
sangone88 <llil1@naver.com>
2019-11-11 19:36:13 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191111_01_dateandtime/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.a20191111_01_dateandtime
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
